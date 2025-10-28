2924a08a911c8c863b4c80511724cba98b028d06
Nam <ptnam1672003@gmail.com>
2025-10-28 22:37:33 +0700

    feat(Api-response): implement api response

---
+++ jobtracker-app/src/main/java/com/jobtracker/jobtracker_app/dto/response/ApiResponse.java
@@ -0,0 +1,23 @@
+package com.jobtracker.jobtracker_app.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ApiResponse<T> {
+    @Builder.Default
+    Boolean success = true;
+    String message;
+    T data;
+    List<ValidationError> errors;
+    @Builder.Default
+    LocalDateTime timestamp = LocalDateTime.now();
+}
